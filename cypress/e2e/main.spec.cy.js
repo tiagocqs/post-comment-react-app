ec079c06250d4d5d9ce496b70d77ebc89060662e
Tiago Queiroz <tiago_cqs@hotmail.com>
2025-05-07 21:41:03 -0300

    First scenario cypress automation

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -2,11 +2,11 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    cy.get('[data-qa="comment-input"]').first().type('Testando 123')
     cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('Test 123')
+    cy.get('[data-qa="comment-text"]').contains('Testando 123')
     cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'Test 123') {
+      if ($element.text() === 'Testando 123') {
         cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
     })
